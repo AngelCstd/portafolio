4e87a148e34af00a313fc0928f11e70307fd0170
angelcstd <luisacast.29@gmail.com>
2024-04-15 17:53:52 -0600

    DONE

--- components/Formacion.js
+++
@@ -1,7 +0,0 @@
-export const Formacion= ({imagen, informacion, titulo}) =>{
-    return`<div class="academico__box">
-        <img src="${imagen}">
-        <h4>${titulo}</h4>
-        <p>${informacion}</p>
-</div>`
-}
